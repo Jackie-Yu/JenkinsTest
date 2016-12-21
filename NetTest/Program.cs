dc71b38706980bc8eaf4097c4452cdab4826fef2
jackie.yu <jackie.yu@missionsky.com>
2016-12-21 17:23:20 +0800

    Init

---
+++ NetTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This jenkins test for .net!");
+            Console.Read();
+        }
+    }
+}
